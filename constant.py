02aa7ac27a3c89ff75c14ca68eb42f010aa86e35
KingBridgeSS <93264909+KingBridgeSS@users.noreply.github.com>
2021-12-16 13:28:19 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -11,54 +11,125 @@ REPORT_API = 'https://app.bupt.edu.cn/ncov/wap/default/save'
 # 当今日没有填报时，在https://app.bupt.edu.cn/ncov/wap/default/index下进行填报，
 # 全部填完，不要提交，f12打开控制台，在Console页面下输入代码 console.log(vm.info) 就会得到以下信息，之后每天就默认填以下信息
 INFO = r"""{
-        "address":"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区",
-        "area":"北京市  海淀区",
-        "bztcyy":"",
-        "city":"北京市",
-        "csmjry":"0",
-        "fjqszgjdq":"",
-        "geo_api_info":"{\"type\":\"complete\",\"position\":{\"Q\":39.960390625,\"R\":116.356397569445,\"lng\":116.356398,\"lat\":39.960391},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":23,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110108\",\"businessAreas\":[{\"name\":\"北下关\",\"id\":\"110108\",\"location\":{\"Q\":39.955976,\"R\":116.33873,\"lng\":116.33873,\"lat\":39.955976}},{\"name\":\"西直门\",\"id\":\"110102\",\"location\":{\"Q\":39.942856,\"R\":116.34666099999998,\"lng\":116.346661,\"lat\":39.942856}},{\"name\":\"小西天\",\"id\":\"110108\",\"location\":{\"Q\":39.957147,\"R\":116.364058,\"lng\":116.364058,\"lat\":39.957147}}],\"neighborhoodType\":\"科教文化服务;学校;高等院校\",\"neighborhood\":\"北京邮电大学\",\"building\":\"北京邮电大学计算机学院\",\"buildingType\":\"科教文化服务;学校;高等院校\",\"street\":\"西土城路\",\"streetNumber\":\"10号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"海淀区\",\"township\":\"北太平庄街道\"},\"formattedAddress\":\"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}",
-        "glksrq":"",
-        "gllx":"",
-        "gtjzzchdfh":"",
-        "gtjzzfjsj":"",
-        "ismoved":"0",
-        "jcbhlx":"",
-        "jcbhrq":"",
-        "jchbryfs":"",
-        "jcjgqr":"0",
-        "jcwhryfs":"",
-        "jhfjhbcc":"",
-        "jhfjjtgj":"",
-        "jhfjrq":"",
-        "mjry":"0",
-        "province":"北京市",
-        "qksm":"",
-        "remark":"",
-        "sfcxtz":"0",
-        "sfcxzysx":"0",
-        "sfcyglq":"0",
-        "sfjcbh":"0",
-        "sfjchbry":"0",
-        "sfjcwhry":"0",
-        "sfjzdezxgym":"1",
-        "sfjzxgym":"1",
-        "sfsfbh":"0",
-        "sftjhb":"0",
-        "sftjwh":"0",
-        "sfxk":"0",
-        "sfygtjzzfj":"",
-        "sfyyjc":"0",
-        "sfzx":1,
-        "szcs":"",
-        "szgj":"",
-        "szsqsfybl":"0",
-        "tw":"2",
-        "xjzd":"",
-        "xkqq":"",
-        "xwxgymjzqk":"3",
-        "ymjzxgqk":"已接种",
-        "zgfxdq":"0"
+        address: "北京市昌平区沙河镇北京邮电大学沙河校区雁北园男生宿舍"
+​
+area: "北京市  昌平区"
+​
+bztcyy: ""
+​
+city: "北京市"
+​
+created: 1639585453
+​
+created_uid: 0
+​
+csmjry: "0"
+​
+date: "20211216"
+​
+geo_api_info: "{\"type\":\"complete\",\"position\":{\"Q\":40.157922634549,\"R\":116.28848578559098,\"lng\":116.288486,\"lat\":40.157923},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":76,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110114\",\"businessAreas\":[{\"name\":\"沙河\",\"id\":\"110114\",\"location\":{\"Q\":40.134237,\"R\":116.28009600000001,\"lng\":116.280096,\"lat\":40.134237}},{\"name\":\"百善\",\"id\":\"110114\",\"location\":{\"Q\":40.175749,\"R\":116.32030500000002,\"lng\":116.320305,\"lat\":40.175749}}],\"neighborhoodType\":\"\",\"neighborhood\":\"\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"南丰路\",\"streetNumber\":\"1号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"昌平区\",\"towncode\":\"110114123000\",\"township\":\"沙河镇\"},\"formattedAddress\":\"北京市昌平区沙河镇北京邮电大学沙河校区雁北园男生宿舍\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}"
+​
+glksrq: ""
+​
+gllx: ""
+​
+gtjzzfjsj: ""
+​
+gwszdd: ""
+​
+id: 16000670
+​
+ismoved: 0
+​
+jcbhlx: ""
+​
+jcbhrq: ""
+​
+jchbryfs: ""
+​
+jcjg: ""
+​
+jcjgqr: "0"
+​
+jcqzrq: ""
+​
+jcwhryfs: ""
+​
+jhfjhbcc: ""
+​
+jhfjjtgj: ""
+​
+jhfjrq: ""
+​
+jrsfqzfy: ""
+​
+jrsfqzys: ""
+​
+mjry: "0"
+​
+province: "北京市"
+​
+qksm: ""
+​
+remark: ""
+​
+sfcxtz: "0"
+​
+sfcxzysx: "0"
+​
+sfcyglq: "0"
+​
+sfjcbh: "0"
+​
+sfjchbry: "0"
+​
+sfjcqz: ""
+​
+sfjcwhry: "0"
+​
+sfjzdezxgym: 1
+​
+sfjzxgym: 1
+​
+sfsfbh: 0
+​
+sfsqhzjkk: 0
+​
+sftjhb: "0"
+​
+sftjwh: "0"
+​
+sfxk: 0
+​
+sfygtjzzfj: 0
+​
+sfyqjzgc: ""
+​
+sfyyjc: "0"
+​
+sfzx: 1
+​
+sqhzjkkys: ""
+​
+szcs: ""
+​
+szgj: ""
+​
+szsqsfybl: 0
+​
+tw: "3"
+​
+uid: "83242"
+​
+xjzd: ""
+​
+xkqq: ""
+​
+xwxgymjzqk: 3
+​
+ymjzxgqk: ""
+​
+zgfxdq: "0"
         }"""
 
 REASONABLE_LENGTH = 24
